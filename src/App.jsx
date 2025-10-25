64adcf5464c448c487c90d4cb8dc11f96498e3fe
Kimmy Tran <kimmytraan@gmail.com>
2025-10-25 17:33:44 -0400

    sending changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ import React, { useEffect, useMemo, useRef, useState } from "react";
 
 /* === Spotify OAuth (via your backend) === */
 const SPOTIFY_CLIENT_ID = "59a1a68551ab4e0aa819450939ac3219";
-const SPOTIFY_REDIRECT_URI = "http://127.0.0.1:5174/callback";
+const SPOTIFY_REDIRECT_URI = "https://moododoro-jyhdiz3wv-ellie-kim1s-projects.vercel.app/callback";
 const SPOTIFY_SCOPES = ["user-read-private", "playlist-read-private"]; // server must add streaming scopes
 
-const BACKEND_URL = "http://127.0.0.1:5174";
+const BACKEND_URL = "https://moododoro-jyhdiz3wv-ellie-kim1s-projects.vercel.app/api/spotify";
 function connectSpotify() {
   window.location.href = `${BACKEND_URL}/login`;
 }
